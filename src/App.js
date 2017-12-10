6b8880e03c9fa3756a56468e14e455aecc88b350
Piotr Ferens <piotrferens@outlook.com>
2017-12-10 17:33:22 +0100

    filtering emojis by input text

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React, { Component } from "react";
 import emojiJson from "./emojiList";
 
 class App extends Component {
+  state = {
+    emojis: emojiJson.slice(0, 30),
+    searchText: ""
+  };
+
+  onSearch = event => {
+    const value = event.target.value;
+    const filteredEmojis = emojiJson.filter(emoji =>
+      emoji.title.toLowerCase().includes(value.toLowerCase())
+    );
+    this.setState({ searchText: value, emojis: filteredEmojis.slice(0, 30) });
+  };
+
   render() {
     return (
       <div>
         <h1 style={{ textAlign: "center" }}>💪Emoji search💪</h1>
-        <input style={{ width: "100%", marginBottom: 50 }} />
-        {emojiJson.map(emoji => (
+        <input
+          style={{ width: "100%", marginBottom: 50 }}
+          onChange={this.onSearch}
+          value={this.state.searchText}
+        />
+        {this.state.emojis.map(emoji => (
           <div
             style={{
               borderBottom: "1px solid grey",
