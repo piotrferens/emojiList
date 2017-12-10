af7fa404856dc2dec7d8d41842efc803d6e9a1bb
Piotr Ferens <piotrferens@outlook.com>
2017-12-10 17:07:53 +0100

    added styles to list

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        {emojiJson.map(emoji => <div key={emoji.title}>{emoji.symbol}</div>)}
+        <h1 style={{ textAlign: "center" }}>💪Emoji search💪</h1>
+        <input style={{ width: "100%" }} />
+        {emojiJson.map(emoji => (
+          <div style={{ borderBottom: "1px solid grey" }} key={emoji.title}>
+            {emoji.symbol}
+          </div>
+        ))}
       </div>
     );
   }
